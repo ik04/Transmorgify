4296fd56a18e3e5e601446741d51380e7728327b
ishaan khurana <ishaankhurana123@gmail.com>
2024-06-01 15:10:46 +0530

    feat: finish link bar

--- client/web/app/page.js
+++ client/web/app/page.js
@@ -37,17 +37,19 @@ const page = () => {
 
   return (
     <div className="">
-      <div className="h-screen bg-main flex flex-col space-y-3 justify-center items-center">
-        <div className="title flex justify-center items-center gap-3">
-          <Image src={"/assets/wave-2.svg"} width={100} height={100} />
-          <h1 className="text-stroke-xl font-display md:text-[120px] text-center text-main text-stroke-heliotrope uppercase tracking-widest">
-            Transmorgify
-          </h1>
-          <Image src={"/assets/wave-2.svg"} width={100} height={100} />
-        </div>
-        <div className="slogan font-base text-mediumSlateBlue text-center md:text-5xl">
-          “Morph your <span className="font-display uppercase">links</span> into
-          melody”
+      <div className="h-screen bg-main flex flex-col justify-center items-center space-y-16">
+        <div className="title-and-slogan flex flex-col justify-center items-center space-y-3">
+          <div className="title flex justify-center items-center gap-3">
+            <Image src={"/assets/wave-2.svg"} width={100} height={100} />
+            <h1 className="text-stroke-xl font-display md:text-[120px] text-center text-main text-stroke-heliotrope uppercase tracking-widest">
+              Transmorgify
+            </h1>
+            <Image src={"/assets/wave-2.svg"} width={100} height={100} />
+          </div>
+          <div className="slogan font-base text-mediumSlateBlue text-center md:text-5xl">
+            “Morph your <span className="font-display uppercase">links</span>{" "}
+            into melody”
+          </div>
         </div>
         <div className="bg-gradient-to-br  from-[#CF5FCD] via-[#CF5FCD] via-40% to-mediumSlateBlue md:h-28 md:w-[63%] rounded-full flex items-center px-5">
           <label htmlFor="link">
@@ -66,6 +68,17 @@ const page = () => {
             className="bg-transparent w-full md:text-3xl px-2 focus:outline-none font-display placeholder:text-main placeholder:font-display placeholder:font-bold"
             onChange={(e) => setLink(e.target.value)}
           />
+          <div className="convert mb-2 flex items-center">
+            <h2 className="text-stroke-lg text-transparent text-stroke-main font-base md:text-3xl">
+              Morph
+            </h2>
+            <Image
+              src={"/assets/double-arrow.svg"}
+              className="mt-1"
+              width={70}
+              height={70}
+            />
+          </div>
         </div>
       </div>
     </div>
